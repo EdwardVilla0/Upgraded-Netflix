95962e0c6486ba50d8c48fd8ea30643dd45c0743
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-16 14:37:31 -0800

    adding styling to components

--- src/components/HomeScreen/HomeScreen.component.js
+++ src/components/HomeScreen/HomeScreen.component.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import './HomeScreen.styles.css'
+import './HomeScreen.styles.css';
+import Nav from '../Nav/Nav.component';
 
 function HomeScreen() {
 
     return (
         <div className="homeScreen">
-
+            <Nav />
         </div>
     )
 }
