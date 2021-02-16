5bf258b6298553aaac7940a8900f8ccea04d3626
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-16 13:32:29 -0800

    cleaning up project to start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC2L3CWn0ABGXm25W7AZyXxc69N1dGKCUE",
+    authDomain: "netflixupgrade-91270.firebaseapp.com",
+    projectId: "netflixupgrade-91270",
+    storageBucket: "netflixupgrade-91270.appspot.com",
+    messagingSenderId: "597154363989",
+    appId: "1:597154363989:web:b9a5016da7450fe2adb2dc",
+    measurementId: "G-T16CT0P9FW"
+};
