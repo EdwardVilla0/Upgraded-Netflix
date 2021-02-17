624ffad699a3b45091e4e5e4297be0a04f0be8c9
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-17 14:46:22 -0800

    retrieving data from api

--- src/components/Banner/Banner.components.js
+++ src/components/Banner/Banner.components.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from '../../axios';
+import requests from '../../Requests';
 import './Banner.styles.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
 
-    // useEffect(() => {
-    //     async function fetchData() {
-    //         const request = await axios.get(requests.fetchNetflixOriginals);
-    //         setMovie(
-    //             request.data.results[
-    //             Math.floor(Math.random() * request.data.results.length - 1)
-    //             ]
-    //         );
-    //         return request;
-    //     }
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
 
-    //     fetchData()
-    // }, [])
+        fetchData()
+    }, [])
 
     function truncate(str, n) {
         return str?.length > n ? str.substr(0, n - 1) + "..." : str;
@@ -26,13 +28,13 @@ function Banner() {
         <header className="banner"
             style={{
                 backgroundSize: "cover",
-                // backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
                 backdropPosition: "center center",
             }}>
             <div className="banner__contents">
                 {/* title */}
                 <h1 className="banner__title">
-                    {/* {movie?.title || movie?.name || movie?.original_name} */}
+                    {movie?.title || movie?.name || movie?.original_name}
                 </h1>
 
                 {/* 2 buttons */}
@@ -42,8 +44,8 @@ function Banner() {
                 </div>
 
 
-                {/* description */}
-                {/* <h1 className="banner__description">{truncate(movie?.overview, 200)}</h1> */}
+
+                <h1 className="banner__description">{truncate(movie?.overview, 200)}</h1>
             </div>
             <div className="banner__fadeBottom" />
         </header>
