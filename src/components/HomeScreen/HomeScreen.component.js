878ca3b45efeaeae3d9f4dfd757486059e7600b0
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-17 15:15:23 -0800

    adding row component and styling the home screen

--- src/components/HomeScreen/HomeScreen.component.js
+++ src/components/HomeScreen/HomeScreen.component.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './HomeScreen.styles.css';
 import Nav from '../Nav/Nav.component';
 import Banner from '../Banner/Banner.components';
+import Row from '../Row/Row.component';
+import request from '../../Requests';
 
 function HomeScreen() {
 
@@ -9,6 +11,17 @@ function HomeScreen() {
         <div className="homeScreen">
             <Nav />
             <Banner />
+            <Row
+                title="NETFLIX ORIGINALS"
+                fetchUrl={request.fetchNetflixOriginals}
+                isLargeRow />
+            <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+            <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+            <Row title="Action" fetchUrl={request.fetchActionMovies} />
+            <Row title="Comedy" fetchUrl={request.fetchComedyMovies} />
+            <Row title="Horror" fetchUrl={request.fetchHorrorMovies} />
+            <Row title="Romance" fetchUrl={request.fetchRomanceMovies} />
+            <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
         </div>
     )
 }
