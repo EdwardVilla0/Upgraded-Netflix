4e7cc0644a29615701442eea603a6d8eb8e626da
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-16 18:08:15 -0800

    adding banner to home component

--- src/components/HomeScreen/HomeScreen.component.js
+++ src/components/HomeScreen/HomeScreen.component.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './HomeScreen.styles.css';
 import Nav from '../Nav/Nav.component';
+import Banner from '../Banner/Banner.components';
 
 function HomeScreen() {
 
     return (
         <div className="homeScreen">
             <Nav />
+            <Banner />
         </div>
     )
 }
