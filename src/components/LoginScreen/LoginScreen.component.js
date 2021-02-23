a56256d243539bc157e206434793afa0b5d4e0f8
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-22 19:34:31 -0800

    styling the button on login

--- src/components/LoginScreen/LoginScreen.component.js
+++ src/components/LoginScreen/LoginScreen.component.js
@@ -5,8 +5,14 @@ function LoginScreen() {
     return (
         <div className="loginScreen">
             <div className="loginScreen__background">
-                <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                <img
+                    className="loginScreen__logo"
+                    src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
                     alt="logo" />
+
+                <button className="loginScreen__button">
+                    Sign In
+                </button>
             </div>
         </div>
     )
