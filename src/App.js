f3803da296d42cfd75de3aa3c7185d46897952b6
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-22 19:15:58 -0800

    creating login page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen/HomeScreen.component';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './components/LoginScreen/LoginScreen.component';
 
 function App() {
+  const user = null;
+
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (<LoginScreen />) : (
+          <Switch>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+
       </Router>
 
     </div>
---
+++ src/components/LoginScreen/LoginScreen.component.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginScreen.styles.css'
+
+function LoginScreen() {
+    return (
+        <div className="loginScreen">
+            <div className="loginScreen__background">
+                <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="logo" />
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
