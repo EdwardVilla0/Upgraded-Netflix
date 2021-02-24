58b09000950a9aadf32ce005238c46be56b853e7
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-23 19:09:42 -0800

    changing the background of the login screen and adding form

--- src/components/LoginScreen/LoginScreen.component.js
+++ src/components/LoginScreen/LoginScreen.component.js
@@ -13,7 +13,21 @@ function LoginScreen() {
                 <button className="loginScreen__button">
                     Sign In
                 </button>
-                <div className="loginScreen__gradient"></div>
+                <div className="loginScreen__gradient" />
+            </div>
+            <div className="loginScreen__body">
+                <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at any time.</h2>
+                    <h3>Ready to watch? Enter your email to create or restard your membership</h3>
+
+                    <div className="loginScreen__input">
+                        <form>
+                            <input type='email'
+                                placeholder="Email Address" />
+                        </form>
+                    </div>
+                </>
             </div>
         </div>
     )
