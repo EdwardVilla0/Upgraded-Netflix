9fd4a63a476a02a77dba66b1a9349e6aa361c6b1
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-02-26 13:12:48 -0800

    navigating to the profile screen

--- src/components/Nav/Nav.component.js
+++ src/components/Nav/Nav.component.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Nav.styles.css';
 
 function Nav() {
     const [show, handleShow] = useState(false);
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 100) {
-                handleShow(true);
-            } else {
-                handleShow(false);
-            }
-        });
-        return () => {
-            window.removeEventListener("scroll");
-        };
-    }, []);
-    return (
-        <div className={`nav ${show && "nav__black"}`}>
-            <img
-                className="nav__logo"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
-                alt="Neflix logo" />
+    const history = useHistory();
 
-            <img
-                className="nav__avatar"
-                src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
-                alt="Avatar"
-            />
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.addEventListener("scroll", transitionNavBar);
+    }, [])
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <div className="nav__contents">
+                <img
+                    onClick={() => history.push("/")}
+                    className="nav__logo"
+                    src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                    alt="netflix-logo"
+                />
+                <img
+                    onClick={() => history.push("/profile")}
+                    className="nav__avatar"
+                    src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                    alt="netflix-avatar"
+                />
+            </div>
         </div>
     )
 }
