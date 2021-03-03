49e5f3711a7416b24b7c6043a5569549eeb52a96
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-02 18:41:46 -0800

    fixing price id

--- src/components/PlansScreen/PlansScreen.component.js
+++ src/components/PlansScreen/PlansScreen.component.js
@@ -1,24 +1,42 @@
-import { loadStripe } from '@stripe/stripe-js';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { loadStripe } from '@stripe/stripe-js';
+
 import { selectUser } from '../../features/userSlice';
 import db from '../../firebase';
+
 import './PlansScreen.styles.css';
 
 function PlansScreen() {
-    const [products, setProducts] = useState([]);
     const user = useSelector(selectUser);
+    const [products, setProducts] = useState([]);
+    const [subscription, setSubscription] = useState(null);
+
+    useEffect(() => {
+        db.collection('customers')
+            .doc(user.uid)
+            .collection('subscriptions')
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(async subscription => {
+                    setSubscription({
+                        role: subscription.data().role,
+                        current_period_end: subscription.data().current_period_end.seconds,
+                        current_period_start: subscription.data().current_period_start.seconds,
+                    })
+                })
+            })
+    }, [user.uid]);
 
     useEffect(() => {
         db.collection('products')
-            .where("active", "==", true)
-            .get()
-            .then((querySnapshot) => {
+            .where('active', '==', true)
+            .get().then(querySnapshot => {
                 const products = {};
                 querySnapshot.forEach(async productDoc => {
                     products[productDoc.id] = productDoc.data();
                     const priceSnap = await productDoc.ref.collection('prices').get();
-                    priceSnap.docs.forEach((price) => {
+                    priceSnap.docs.forEach(price => {
                         products[productDoc.id].prices = {
                             priceId: price.id,
                             priceData: price.data()
@@ -26,48 +44,72 @@ function PlansScreen() {
                     })
                 });
                 setProducts(products);
-            })
+            });
     }, []);
+
+
 
     const loadCheckout = async (priceId) => {
         const docRef = await db
             .collection('customers')
             .doc(user.uid)
-            .collection("")
+            .collection('checkout_sessions')
             .add({
                 price: priceId,
                 success_url: window.location.origin,
-                cancel_url: window.location.origin
+                cancel_url: window.location.origin,
             });
 
         docRef.onSnapshot(async (snap) => {
             const { error, sessionId } = snap.data();
 
             if (error) {
-                alert(`an error has occured: ${error.message}`)
+                alert(`An error occured: ${error.message}`);
             }
 
             if (sessionId) {
-                const stripe = await loadStripe()
+                const stripe = await loadStripe("pk_test_51HoyUEFnGzCsbM6D0Mg3iUHGqsKyNtFbHfgkp6YCOQeePtuVKT2K4sQB2cNT8gJUMTZrbtsOGukqd85XGPklhuEy00oP0Zz6xv");
+                stripe.redirectToCheckout({ sessionId });
             }
-        })
+        });
     };
 
     return (
         <div className="plansScreen">
+            {subscription && (
+                <p>Renewal date:{" "}
+                    {new Date(subscription
+                        ?.current_period_end * 1000)
+                        .toLocaleDateString()}</p>
+            )}
             {Object.entries(products).map(([productId, productData]) => {
+
+                const isCurrentPackage = productData.name
+                    ?.toLowerCase()
+                    .includes(subscription?.role);
+
                 return (
-                    <div className="plansScreen__plan">
+                    <div
+                        key={productId}
+                        className={`${isCurrentPackage && "plansScreen__plan--disabled"
+                            } plansScreen__plan`}
+                    >
                         <div className="plansScreen__info">
                             <h5>{productData.name}</h5>
-                            <h5>{productData.description}</h5>
+                            <h6>{productData.description}</h6>
                         </div>
-                        <button onClick={() => loadCheckout(productData.prices.priceId)}>Subscribe</button>
+
+                        <button onClick={() =>
+                            !isCurrentPackage && loadCheckout(productData.prices.priceId)
+                        }
+                        >
+                            {isCurrentPackage ? 'Current' : 'Subscribe'}
+                        </button>
                     </div>
-                );
+                )
             })}
         </div>
     )
 }
 
-export default PlansScreen
+export default PlansScreen;
